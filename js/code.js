76f8ded0debd498cecc91141facf58f5d958e0e7
Boris Trochez <boris.trochez@gmail.com>
2020-02-22 18:59:01 -0500

    funcionalidad al boton dont push me

--- js/code.js
+++ js/code.js
@@ -3,6 +3,17 @@ document.getElementById("pushme").addEventListener("click", showAMessage);
 
 function showAMessage()
 {
+    
+        document.getElementById("m1").style.display = "block";
+        document.getElementById("m1").innerHTML = "Thank you! I needed to breath!"
+    
+    
+}
+
+document.getElementById("dontpushme").addEventListener("click",showAMessage2);
+
+function showAMessage2()
+{
     document.getElementById("m1").style.display = "block";
-    document.getElementById("m1").innerHTML = "Thank you! I needed to breath!"
+    document.getElementById("m1").innerHTML = "I said: Don't push me! Now you'll suffer the consequences..."
 }
